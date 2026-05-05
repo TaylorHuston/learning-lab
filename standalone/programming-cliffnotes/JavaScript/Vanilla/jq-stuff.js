65ce03f3e7e0a923e6ded8ed81e8f62cf84abab8
Taylor Huston <your_email@example.com>
2026-05-05 16:57:32 -0400

    Update jQuery examples

--- standalone/programming-cliffnotes/JavaScript/Vanilla/jq-stuff.js
+++ standalone/programming-cliffnotes/JavaScript/Vanilla/jq-stuff.js
@@ -1,60 +1,56 @@
-$(document).ready(function () { //Function is an event handler that happens when the document is ready
+// jQuery examples
+// jQuery is not required for modern vanilla JavaScript, but you may still see it
+// in older projects. These examples show common legacy patterns.
 
-  //Can select with elemeny type, class, or id.  Essentially same as using CSS selectors.
+$(function () {
+  // Runs when the document is ready.
+
+  // Select by element, class, or id. jQuery uses CSS selector syntax.
   $("h1");
   $(".myClass");
   $("#myId");
 
+  $("h1").text("This header was changed by jQuery");
 
-  $('h1').text("This header was changed by jQuery");
+  // You can also find elements by traversing.
+  $("#myList").find("li"); // Same as $("#myList li")
+  $("#myList").children("li"); // Same as $("#myList > li")
+  $("#myList").find("li").first().next(); // Second list item
+  $("#myList").find("li").first().parent(); // The #myList ul
+  $("#myList").closest(".text"); // First ancestor with that class
 
-  //You can also find elements by traversing
-  $("#myList").find("li"); //Essentially same as $("myList li");
-  $("#myList").children("li"); //Essentially same as $("myList > li");
-  $("#myList").find("li").first().next(); //Second list item
-  $("#myList").find("li").first().parent(); //The myList UL
-  $("#myList").closest(".text"); //First ancestor with that class
+  $(".class1").filter(".class2"); // Everything with class1 AND class2
 
-  $(".class1").filter(".class2"); //Everything with class1 AND class2
-
-  //Launch external JS functions
-  $('#jQuery').on('click', function () {
-    alert("JQ Button pushed");
+  // Register event listeners.
+  $("#jQuery").on("click", function () {
+    console.log("jQuery button pushed");
   });
 
-  //Add content
-  $("#mainParagraph").before('Before '); //Before siblng
-  $("#mainParagraph").after('After '); //After sibling
-  $("#mainParagraph").prepend('Prepend '); //First child
-  $("#mainParagraph").append('Append '); //Last child
+  // Add content.
+  $("#mainParagraph").before("Before "); // Before sibling
+  $("#mainParagraph").after("After "); // After sibling
+  $("#mainParagraph").prepend("Prepend "); // First child
+  $("#mainParagraph").append("Append "); // Last child
 
-  //Add class
-  $('#mainParagraph').addClass("jqueryclass");
-  $('li').addClass("liclass");
+  // Add classes.
+  $("#mainParagraph").addClass("jqueryclass");
+  $("li").addClass("liclass");
 
-  $('#clickToShow').on('click', function () {
-    $('#slideout').slideToggle();
-    $('#fadein').fadeToggle();
+  $("#clickToShow").on("click", function () {
+    $("#slideout").slideToggle();
+    $("#fadein").fadeToggle();
   });
-  //There are lots of other mouse events like mouseEnter and mouseLeave
-  //There are keyboard events like keydown and keyup
 
-  $('h2').on('click', function () {
+  // There are many other mouse and keyboard events, like mouseenter, mouseleave,
+  // keydown, and keyup.
+  $("h2").on("click", function () {
     $(this).text("Clicked!");
   });
 
-  //  var price = +$(this).data('price'); //Get the string from the data-price tag and convert it to a number
-  //  var quantity = +$(this).val(); //Value from a field
-  //
-  //  $(this).css({
-  //    '<attr>': '<value>'
-  //  }); //Add a CSS rule
-  //
-  //  $(this).toggleClass('<class>'); //Add/remove a CSS class
-  //
-  //  $(this).animate({
-  //    '<direction>': ,
-  //    '<amount>'
-  //  }, < speed > );
-
+  // Other common jQuery patterns:
+  // const price = Number($(this).data("price")); // Convert data-price to a number
+  // const quantity = Number($(this).val()); // Convert a field value to a number
+  // $(this).css({ color: "red" }); // Add a CSS rule
+  // $(this).toggleClass("active"); // Add/remove a CSS class
+  // $(this).animate({ opacity: 0.5 }, 300); // Animate over 300ms
 });
