c2b26b428be686b0b0b5d14b51bd294c63781e89
Taylor Huston <your_email@example.com>
2026-05-05 17:03:15 -0400

    Update timer examples

--- standalone/programming-cliffnotes/JavaScript/Vanilla/timers.js
+++ standalone/programming-cliffnotes/JavaScript/Vanilla/timers.js
@@ -1,28 +1,37 @@
-//Timer related events in JS
+// Timer-related events in JavaScript
 
 function simpleMessage() {
   console.log("5 seconds have passed");
 }
 
-//Run once after 5 seconds
+// Run once after 5 seconds. The delay is in milliseconds.
 setTimeout(simpleMessage, 5000);
 
-var myImage = document.getElementById("slideImage");
-var images = ["_images/cat1.jpg", "_images/cat2.jpg", "_images/cat3.jpg"];
-var slideIndex = 0;
+const myImage = document.getElementById("slideImage");
+const images = ["_images/cat1.jpg", "_images/cat2.jpg", "_images/cat3.jpg"];
+let slideIndex = 0;
+let myInterval;
 
 function slideshow() {
   myImage.setAttribute("src", images[slideIndex]);
-  slideIndex++;
-  if (slideIndex >= images.length) {
-    slideIndex = 0;
-  }
+  slideIndex = (slideIndex + 1) % images.length;
 }
 
-//Run every 5 seconds
-var myInterval = setInterval(slideshow, 5000);
+if (myImage) {
+  slideshow();
 
-//Stop the interval if user clicks on image
-myImage.onclick = function () {
-  clearInterval(myInterval);
+  // Run every 5 seconds until the interval is cleared.
+  myInterval = setInterval(slideshow, 5000);
+
+  // Stop the interval if the user clicks the image.
+  myImage.addEventListener("click", function () {
+    clearInterval(myInterval);
+    console.log("Slideshow stopped");
+  });
 }
+
+// clearTimeout() cancels a timeout that has not run yet.
+const timeoutId = setTimeout(function () {
+  console.log("This message will not run");
+}, 1000);
+clearTimeout(timeoutId);
