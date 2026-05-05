917eea484f97c94858a61147877f031aba5302b8
Taylor Huston <your_email@example.com>
2026-05-05 16:55:36 -0400

    Update function examples

--- standalone/programming-cliffnotes/JavaScript/Vanilla/functions.js
+++ standalone/programming-cliffnotes/JavaScript/Vanilla/functions.js
@@ -1,114 +1,108 @@
-//Functions
+// Functions
 
 function functions() {
-  //Standard functions are hoisted when the file is first loaded, can be used before they are defined
-  let x = makeANumber();
-  console.log(x); //72
+  // Function declarations are hoisted, so they can be called before they appear in the file.
+  const x = makeANumber();
+  console.log(x); // 72
 
   function makeANumber() {
     return 72;
   }
 
-  let name = "Taylor";
+  const name = "Taylor";
 
-  //Anonymous functions are not hoisted, must be defined in the code before they are used
-  let sayHi = function (name) {
-    console.log("Nice to meet you " + name);
+  // Function expressions must be assigned before they are called.
+  const sayHi = function (name) {
+    console.log(`Nice to meet you ${name}`);
   };
 
   sayHi(name);
 
-  //Standard local variable scope applies
-  let myScope = 10;
+  // Standard local variable scope applies.
+  const myScope = 10;
 
   function simpleFunction() {
-    let myScope = 20;
-    console.log(myScope); //20
+    const myScope = 20;
+    console.log(myScope); // 20
   }
   simpleFunction();
-  console.log(myScope); //10
+  console.log(myScope); // 10
 
-  let adder = function (x, y) {
-    return x + y;
-  };
+  const adder = (x, y) => x + y;
 
-  let z = adder(2, 3);
-  console.log(z); //5
+  const z = adder(2, 3);
+  console.log(z); // 5
 
-  //Functions can return other functions and assign them
+  // Functions can return other functions and assign them.
   function multiplier(factor) {
     return function (number) {
       return number * factor;
     };
   }
 
-  let twice = multiplier(2); //Returns an instance of the nested function above with number = 2;
+  const twice = multiplier(2); // Returns a function that multiplies by 2.
   console.log(twice(5));
 
-  //Call the returned function automatically
-  let third = multiplier(3)(5);
+  // Call the returned function immediately.
+  const third = multiplier(3)(5);
   console.log(third);
 
-  //Standard recursion rules apply
-  function power(base, expon) {
-    if (expon == 0) {
+  // Standard recursion rules apply.
+  function power(base, exponent) {
+    if (exponent === 0) {
       return 1;
-    } else {
-      return base * power(base, expon--);
     }
-  }
 
-  //Functions always have access to a special 'arguments' variable
-  function someArgs(myBool) {
-    let someBool = myBool;
-    for (let i = 1; i < arguments.length; i++) {
-      console.log(arguments[i]);
+    return base * power(base, exponent - 1);
+  }
+  console.log(power(2, 3)); // 8
+
+  // Rest parameters are the modern way to accept any number of arguments.
+  function someArgs(myBool, ...values) {
+    const someBool = myBool;
+    console.log(someBool);
+    for (const value of values) {
+      console.log(value);
     }
-    return;
   }
   someArgs(true, 1, 2, 3);
 
-  function sum() {
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-      total += arguments[i];
-    }
-    return total;
+  function sum(...numbers) {
+    return numbers.reduce((total, number) => total + number, 0);
   }
   console.log(sum(1, 2, 3));
 
-  //Passing functions to other functions is very powerful
-  let anotherArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  //In this example you can use any function in the palce of test to filter on any criteria (note that filter is a standard array method, only defined here manually as an example)
+  // Passing functions to other functions is very powerful.
+  const anotherArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // In this example you can use any function in place of test to filter on any criteria.
+  // Array.prototype.filter() already exists; this is just a manual implementation.
   function filter(array, test) {
-    let passed = [];
-    for (let i = 0; i < array.length; i++) {
-      if (test(array[i])) {
-        passed.push(array[i]);
+    const passed = [];
+    for (const item of array) {
+      if (test(item)) {
+        passed.push(item);
       }
     }
     return passed;
   }
 
-  //Pass anonymous function to filter, in this case one that test for even numbers
+  // Pass an anonymous function to filter, in this case one that tests for even numbers.
   console.log(
-    filter(anotherArray, function (number) {
-      return number % 2 == 0;
-    })
+    filter(anotherArray, (number) => number % 2 === 0)
   );
 
-  //Apply allows you to call a function and specify the 'this', followed by an array of args
+  // apply() allows you to call a function and specify this, followed by an array of arguments.
   function speak(x, y) {
-    console.log("I am a " + this.type + ", " + x + " " + y);
+    console.log(`I am a ${this.type}, ${x} ${y}`);
   }
 
-  let someObject = {
+  const someObject = {
     type: "Human",
   };
 
   speak.apply(someObject, [10, 12]);
 
-  //Call is similar, but doesn't take the arguments as an array
+  // call() is similar, but takes arguments individually instead of as an array.
   speak.call(
     {
       type: "Cat",
@@ -117,14 +111,13 @@ function functions() {
     47
   );
 
-  // Arrow functions are a more concise syntax for writing functions, they do not have their own 'this' context
-  const square = (x) => {
-    return x * x;
-  };
+  // Arrow functions are a concise syntax and do not have their own this context.
+  const square = (x) => x * x;
+  console.log(square(4)); // 16
 
   const person1 = {
     name: "Taylor",
-    sayHi: function () {
+    sayHi() {
       console.log("Hi, my name is", this.name);
     },
   };
